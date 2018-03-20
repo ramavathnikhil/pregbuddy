7695889d5351ccbd0756f7c175c01c520dd130c7
nikhilramavath <nikhil@mantralabsglobal.com>
2018-03-20 20:40:16 +0530

    sd

--- api/models.py
+++ api/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Topic(models.Model):
     topic_text = models.CharField(max_length=255, blank=False, unique=True)
-    userId = models.CharField(max_length=255, blank=False)
+    user = models.CharField(max_length=255, blank=False)
     date_created = models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now=True)
 
