24c60179151a6b20454b4c6c6fe5d5aeb5285f8a
nikhilramavath <nikhil@mantralabsglobal.com>
2018-03-20 14:51:07 +0530

    first push

---
+++ api/serializers.py
@@ -0,0 +1,33 @@
+from django.contrib.auth.models import User, Group
+from rest_framework import serializers
+
+from api.models import Author
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('url', 'username', 'email', 'groups')
+
+
+class GroupSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Group
+        fields = ('url', 'name')
+
+class AuthorSerializer(serializers.ModelSerializer):
+    """
+    Serializing all the Authors
+    """
+    class Meta:
+        model = Author
+        fields = ('id', 'first_name', 'last_name')
+
+class BucketlistSerializer(serializers.ModelSerializer):
+    """Serializer to map the Model instance into JSON format."""
+
+    class Meta:
+        """Meta class to map serializer's fields with the model fields."""
+        model = Author
+        fields = ('id', 'name', 'date_created', 'date_modified')
+        read_only_fields = ('date_created', 'date_modified')
