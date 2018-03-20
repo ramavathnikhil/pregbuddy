02278943b3ace844277f69457444f869244d4898
nikhilramavath <nikhil@mantralabsglobal.com>
2018-03-20 16:30:31 +0530

    added requirments

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url, include
 from rest_framework.urlpatterns import format_suffix_patterns
 from .views import CreateView
 
-urlpatterns = {
+urlpatterns = [
     url(r'^bucketlists/$', CreateView.as_view(), name="create"),
-}
+]
